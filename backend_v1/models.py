dce244daa2d2d4195133ecb2b90014ae6b31a1b1
Gaurav Chopra <gmchopra@wisc.edu>
2024-07-24 23:55:22 -0500

    Added models and schemas for backend

--- backend_v1/models.py
+++ backend_v1/models.py
@@ -1,2 +1,96 @@
-from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
+from sqlalchemy import Boolean, Column, ForeignKey, Integer, String, JSON, Text, ARRAY
 from sqlalchemy.orm import relationship
+
+from .database import Base
+
+
+class Teacher(Base):
+    __tablename__ = "teacher"
+
+    id = Column(Integer, primary_key=True)
+    first_name = Column(String(255), index=True)
+    last_name = Column(String(255))
+    email = Column(String(255))
+
+    courses = relationship("Course", back_populates="teacher")
+
+class Course(Base):
+    __tablename__ = "course"
+    
+    id = Column(Integer, primary_key=True)
+    title = Column(String(255), index=True)
+    teacher_id = Column(Integer, ForeignKey('teacher.id'))
+
+    teacher = relationship("Teacher", back_populates="courses")
+    notebooks = relationship("Notebook", back_populates="course")
+
+
+class Notebook(Base):
+    __tablename__ = "notebook"
+    
+    id = Column(Integer, primary_key=True)
+    course_id = Column(Integer, ForeignKey('course.id'))
+    title = Column(String(255), index = True)
+    description = Column(Text)
+
+    course = relationship("Course", back_populates="notebooks")
+    video_chunks = relationship("VideoChunks", back_populates="notebook")
+    text_chunks = relationship("TextChunks", back_populates="notebook")
+    document_chunks = relationship("DocumentChunks", back_populates="notebook")
+    flashcard_chunks = relationship("FlashcardChunks", back_populates="notebook")
+
+class VideoChunks(Base):
+    __tablename__ = "video_chunks"
+    
+    id = Column(Integer)
+    notebook_id = Column(Integer, ForeignKey("notebook.id"))
+    title = Column(String(255), index = True)
+    order = Column(Integer)
+    video_url = Column(Text)
+    subtitle_content = Column(Text)
+    embedding = Column(ARRAY(float))
+    
+    notebook = relationship("Notebook", back_populates="video_chunks")
+
+
+class TextChunks(Base):
+    __tablename__ = "text_chunks"
+
+    id  = Column(Integer)
+    notebook_id  = Column(Integer, ForeignKey("notebook.id"))
+    title  = Column(String(255), index = True)
+    order = Column(Integer)
+    text_content = Column(Text)
+    embedding = Column(ARRAY(float))
+    
+    notebook = relationship("Notebook", back_populates="text_chunks")
+    
+
+
+class DocumentChunks(Base):
+    __tablename__ = "document_chunks"
+
+    id  = Column(Integer)
+    notebook_id  = Column(Integer, ForeignKey("notebook.id"))
+    filename  = Column(String(255), index = True)
+    order = Column(Integer)
+    filepath  = Column(String(255))
+    content_string = Column(Text)
+    embedding = Column(ARRAY(float))
+    
+    notebook = relationship("Notebook", back_populates="document_chunks")
+
+
+class FlashcardChunks(Base):
+    __tablename__ = "flashcard_chunks"
+
+    id  = Column(Integer)
+    notebook_id  = Column(Integer, ForeignKey("notebook.id"))
+    question  = Column(String(255))
+    order = Column(Integer)
+    options = Column(JSON)
+    explanation = Column(Text)
+    answer = Column(Integer)
+    embedding = Column(ARRAY(float))
+    
+    notebook = relationship("Notebook", back_populates="flashcard_chunks")
---
+++ backend_v1/schemas.py
@@ -0,0 +1,82 @@
+from pydantic import BaseModel, Field
+from typing import List, Optional
+
+class VideoChunksSchema(BaseModel):
+    id: int
+    notebook_id: int
+    title: str
+    order: int
+    video_url: str
+    subtitle_content: str
+    embedding: List[float]
+
+    class Config:
+        orm_mode = True
+
+class TextChunksSchema(BaseModel):
+    id: int
+    notebook_id: int
+    title: str
+    order: int
+    text_content: str
+    embedding: List[float]
+
+    class Config:
+        orm_mode = True
+
+class DocumentChunksSchema(BaseModel):
+    id: int
+    notebook_id: int
+    filename: str
+    order: int
+    filepath: str
+    content_string: str
+    embedding: List[float]
+
+    class Config:
+        orm_mode = True
+
+class FlashcardChunksSchema(BaseModel):
+    id: int
+    notebook_id: int
+    question: str
+    order: int
+    options: Optional[dict]
+    explanation: str
+    answer: int
+    embedding: List[float]
+
+    class Config:
+        orm_mode = True
+
+class NotebookSchema(BaseModel):
+    id: int
+    course_id: int
+    title: str
+    description: str
+    video_chunks: List[VideoChunksSchema] = []
+    text_chunks: List[TextChunksSchema] = []
+    document_chunks: List[DocumentChunksSchema] = []
+    flashcard_chunks: List[FlashcardChunksSchema] = []
+
+    class Config:
+        orm_mode = True
+
+class CourseSchema(BaseModel):
+    id: int
+    title: str
+    teacher_id: int
+    notebooks: List[NotebookSchema] = []
+
+    class Config:
+        orm_mode = True
+
+class TeacherSchema(BaseModel):
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    courses: List[CourseSchema] = []
+
+    class Config:
+        orm_mode = True
