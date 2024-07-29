072dd36cdddf7219c2cd0cadd3ad9dd468bbd0fe
AviTirto <avi.tirto@gmail.com>
2024-07-29 13:34:45 +0700

    Image Based Flashcards set up

--- frontend_v3/src/components/NotebookChunks/FlashcardChunk/EditFlashcardChunk.tsx
+++ frontend_v3/src/components/NotebookChunks/FlashcardChunk/EditFlashcardChunk.tsx
@@ -12,39 +12,45 @@ export const EditFlashcardChunk: React.FC<EditFlashcardChunkProps> = ({ children
             <SheetTrigger asChild>
                 {children}
             </SheetTrigger>
-            <SheetContent className="max-w-2xl sm:max-w-2xl flex flex-col space-y-2">
+            <SheetContent className="max-w-2xl sm:max-w-2xl">
                 <SheetHeader>
                     <SheetTitle>Edit Flashcards</SheetTitle>
                     <SheetDescription>
                         Make changes or remove cards in this deck.
                     </SheetDescription>
                 </SheetHeader>
-                <Carousel
-                    orientation="vertical"
-                >
-                    <CarouselContent>
-                        {
-                            flashcards.map(flashcard => {
-                                return (
-                                    <CarouselItem className="p-5 justify-center">
-                                        <Card>
-                                            <textarea className="w-full p-2">{flashcard.question}</textarea>
-                                            {
-                                                flashcard.options.map(option => {
-                                                    return <textarea className="w-full p-2 text-sm">{option}</textarea>
-                                                })
-                                            }
-                                            <textarea className="w-full p-2 text-sm">{flashcard.answer}</textarea>
-                                            <textarea className="w-full p-2 text-sm">{flashcard.explanation}</textarea>
-                                        </Card>
-                                    </CarouselItem>
-                                )
-                            })
-                        }
-                    </CarouselContent>
-                    <CarouselPrevious />
-                    <CarouselNext />
-                </Carousel>
+                <div className="py-12">
+                    <Carousel
+                        opts={{
+                            align: "center",
+                          }}
+                          orientation="vertical"
+                          className="w-full"
+                    >
+                        <CarouselPrevious />
+                        <CarouselNext />
+                        <CarouselContent>
+                            {
+                                flashcards.map(flashcard => {
+                                    return (
+                                        <CarouselItem>
+                                            <Card className="p-5 justify-center">
+                                                <textarea className="w-full p-2">{flashcard.question}</textarea>
+                                                {
+                                                    flashcard.options.map(option => {
+                                                        return <textarea className="w-full p-2 text-sm">{option}</textarea>
+                                                    })
+                                                }
+                                                <textarea className="w-full p-2 text-sm">{flashcard.answer}</textarea>
+                                                <textarea className="w-full p-2 text-sm">{flashcard.explanation}</textarea>
+                                            </Card>
+                                        </CarouselItem>
+                                    )
+                                })
+                            }
+                        </CarouselContent>
+                    </Carousel>
+                </div>
             </SheetContent>
         </Sheet>
     )
